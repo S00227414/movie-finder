90cfe7d68c1f674b28b222a3207c8590e818e3b6
LeviG97 <levi.gilmartin@gmail.com>
2023-03-14 15:55:09 +0000

    Lab commit after completing code from week7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-finder';
+  movieData:IOMDBResponse | undefined;
+  errorMessage: any;
+
+  constructor(private _omdbService: OmdbApiService){}
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+
+        this.movieData=this.movieData;
+        console.log("Director name : " + this.movieData?.Director);
+      }
+    )
+    return false;
+  }
+  
 }
