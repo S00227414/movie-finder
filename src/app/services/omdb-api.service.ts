d9ae21609a4864f74ba7dc5ad29dca32c44d36e7
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-03-21 11:46:14 +0000

    commit Search not working for multiples. Pre-firebase

--- src/app/services/omdb-api.service.ts
+++ src/app/services/omdb-api.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import { catchError, tap } from 'rxjs';
 import { IOMDBResponse } from '../omdbresponse';
+import { IOMDBResponse2 } from '../omdbresponse2';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class OmdbApiService {
 
   private _siteURL="https://www.omdbapi.com/"
   private _key="?apikey=b6a4a20a&t="
+  private _key2="?apikey=b6a4a20a&s="
   constructor(private _http:HttpClient) { }
 
   getMovieData(movieName:string):Observable<IOMDBResponse>{
@@ -23,6 +25,14 @@ export class OmdbApiService {
     );
 
     
+  }
+  getMoviesData(movieName:string, page:number):Observable<IOMDBResponse2> {
+    return this._http.get<IOMDBResponse2>(this._siteURL+ this._key2 + movieName + "&page=" + page)
+    .pipe(
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
   }
   private handleError(err:HttpErrorResponse){
     console.log('OmdbApiService' + err.message);
